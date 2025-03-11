ea95f8b9c06714a011168ae828569d4da5f43c28
AB <toto>
2025-03-11 16:18:33 +0100

    add new project SafetyNet

---
+++ src/test/java/com/ABoc/safetyNet/SafetyNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ABoc.safetyNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
